85229edafaaa560519c532f9c9733b3d0cb8e879
WisawaK <67030215@kmitl.ac.th>
2024-11-21 13:10:37 +0000

    main

--- cell04/ex03/float.py
+++ cell04/ex03/float.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 user_input = input("Give me a number: ")
 number = float(user_input)
 if number.is_integer():
--- cell04/ex04/round_up.py
+++ cell04/ex04/round_up.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import math
 number = float(input("Give me a number: "))
 rounded_number = math.ceil(number)
